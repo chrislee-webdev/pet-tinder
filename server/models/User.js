7263b11e68db36b311067a8c989f82fd7a9e340e
CarlJMcGee <98133059+CarlJMcGee@users.noreply.github.com>
2022-08-02 09:29:55 -0500

    user model done

---
+++ server/models/User.js
@@ -0,0 +1,48 @@
+const { Schema, model } = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [
+        /[A-Z0-9._%+-]+@[A-Z0-9-]+.+.[A-Z]{2,4}/gim,
+        `Must use a valid email address`,
+      ],
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    pets: [String],
+  },
+
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+
+userSchema.pre("save", async function (next) {
+  if (this.isNew || this.isModified("password")) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+  next();
+});
+
+userSchema.virtual("petCount").get(function () {
+  return this.pets.length;
+});
+
+const User = model("User", userSchema);
+
+module.exports = User;
