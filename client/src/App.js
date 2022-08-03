6bb21f88fee598252cedb0395d86aaac3e6bcd26
CarlJMcGee <98133059+CarlJMcGee@users.noreply.github.com>
2022-08-03 18:51:23 -0500

    added token to req headers

--- client/src/App.js
+++ client/src/App.js
@@ -12,13 +12,24 @@ import {
   InMemoryCache,
   createHttpLink,
 } from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 
 const httpLink = createHttpLink({
   uri: "/graphql",
 });
 
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("id_token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
