5fe6f5d80cfe951402e2d7a492e238ecf05e9b0d
CarlJMcGee <98133059+CarlJMcGee@users.noreply.github.com>
2022-08-03 20:08:41 -0500

    Changed for to htmlFor & class to className

--- client/src/components/About/index.js
+++ client/src/components/About/index.js
@@ -1,50 +1,47 @@
-import React from 'react';
-import coverImage from  '../../assets/cover-image.jpeg';
+import React from "react";
+import coverImage from "../../assets/cover-image.jpeg";
 
 function About() {
-    return (
-        <section>
-            <form class="login-form">
-                <div>
-                    <label for="email-login">Email:</label>
-                    <input type="text" id="email-login" />
-                </div>
-                <div>
-                    <label for="password-login">Password:</label>
-                    <input type="password" id="password-login" />
-                </div>
-                <div>
-                    <button type="submit">Login</button>
-                </div>
-            </form>
+  return (
+    <section>
+      <form className="login-form">
+        <div>
+          <label htmlFor="email-login">Email:</label>
+          <input type="text" id="email-login" />
+        </div>
+        <div>
+          <label htmlFor="password-login">Password:</label>
+          <input type="password" id="password-login" />
+        </div>
+        <div>
+          <button type="submit">Login</button>
+        </div>
+      </form>
 
-            <form class="signup-form">
-                <div>
-                    <label for="username-signup">Username:</label>
-                    <input type="text" id="username-signup" />
-                </div>
-                <div>
-                    <label for="email-signup">Email:</label>
-                    <input type="text" id="email-signup" />
-                </div>
-                <div>
-                    <label for="password-signup">Password:</label>
-                    <input type="password" id="password-signup" />
-                </div>
-                <div>
-                    <button type="submit">Signup</button>
-                </div>
-            </form>
-        
-        
-            <img src={coverImage} style={{ width: '50%' }} alt='cover' />
+      <form className="signup-form">
+        <div>
+          <label htmlFor="username-signup">Username:</label>
+          <input type="text" id="username-signup" />
+        </div>
+        <div>
+          <label htmlFor="email-signup">Email:</label>
+          <input type="text" id="email-signup" />
+        </div>
+        <div>
+          <label htmlFor="password-signup">Password:</label>
+          <input type="password" id="password-signup" />
+        </div>
+        <div>
+          <button type="submit">Signup</button>
+        </div>
+      </form>
 
-            {/* title and decription of pet tinder */}
-            <p>
-                Pinder: Puppy Love!
-            </p>
-        </section>  
-    )
+      <img src={coverImage} style={{ width: "50%" }} alt="cover" />
+
+      {/* title and decription of pet tinder */}
+      <p>Pinder: Puppy Love!</p>
+    </section>
+  );
 }
 
 export default About;
