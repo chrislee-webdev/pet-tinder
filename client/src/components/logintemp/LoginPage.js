523f1c809edf9d6466c9c58c03005b4e93b8ade7
CarlJMcGee <98133059+CarlJMcGee@users.noreply.github.com>
2022-08-05 11:47:08 -0500

    added signup form with validation

--- client/src/components/logintemp/LoginPage.js
+++ client/src/components/logintemp/LoginPage.js
@@ -1,18 +1,89 @@
 import React, { useState } from "react";
 import { useMutation } from "@apollo/client";
-import { LOGIN_USER } from "../../utils/mutations";
+import { ADD_USER, LOGIN_USER } from "../../utils/mutations";
 import auth from "../../utils/auth";
 
 export default function LoginPage() {
-  const [submitLogin, { data, loading, error }] = useMutation(LOGIN_USER);
-  const [formData, setForm] = useState({ email: "", password: "" });
+  const [
+    submitLogin,
+    { data: LoginData, loading: loggingIn, error: loginError },
+  ] = useMutation(LOGIN_USER);
+  const [
+    submitSignup,
+    { data: SignupData, loading: creatingUser, error: signupError },
+  ] = useMutation(ADD_USER);
+  const [formData, setForm] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+  const [usernameValid, setUsername] = useState(false);
+  const [emailValid, setEmail] = useState(false);
+  const [passValid, setPass] = useState(false);
+  const [showLoginAlert, setLoginAlert] = useState(false);
+  const [showSignupAlert, setSignupAlert] = useState(false);
+  const [alertMsg, setMsg] = useState(
+    "Must have valid username/email/password"
+  );
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
     setForm({ ...formData, [name]: value });
   };
-  const handleSubmit = async (e) => {
+  const validate = (e) => {
     e.preventDefault();
+    const { name, value } = e.currentTarget;
+    if (value === "") {
+      e.currentTarget.placeholder = `Must have ${e.target.name}`;
+      return;
+    }
+
+    const valTest = {
+      username: /^[a-z0-9]{6,6}$/i,
+      email: /[A-Z0-9._%+-]+@[A-Z0-9-]+.+.[A-Z]{2,4}/gim,
+      password:
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&_])[A-Za-z\d$@$!%*?&_]{6,20}$/,
+    };
+
+    switch (name) {
+      case "email":
+        valTest.email.test(value)
+          ? setEmail(true)
+          : setMsg(`Must have valid ${name}`);
+        if (!emailValid) {
+          setLoginAlert(true);
+        }
+        break;
+
+      case "username":
+        valTest.username.test(value)
+          ? setUsername(true)
+          : setMsg(`Must have valid ${name}`);
+        if (!emailValid) {
+          setLoginAlert(true);
+        }
+        break;
+
+      case "password":
+        valTest.password.test(value)
+          ? setPass(true)
+          : setMsg(`Must have valid ${name}`);
+        if (!emailValid) {
+          setLoginAlert(true);
+        }
+        break;
+    }
+  };
+
+  const handleLoginSubmit = async (e) => {
+    e.preventDefault();
+
+    if (emailValid === false || passValid === false) {
+      return setLoginAlert(true);
+    }
+
+    setLoginAlert(false);
+
     try {
       const {
         data: {
@@ -20,11 +91,11 @@ export default function LoginPage() {
         },
       } = await submitLogin({ variables: { ...formData } });
 
-      if (loading) {
+      if (loggingIn) {
         return <h1>"Loading..."</h1>;
       }
-      if (error) {
-        return console.log(error);
+      if (loginError) {
+        return console.log(loginError);
       }
       if (user) {
         alert(`Logged in`);
@@ -35,15 +106,78 @@ export default function LoginPage() {
       console.error(err);
     }
   };
+
+  const handleSignupSubmit = async (e) => {
+    e.preventDefault();
+
+    if (
+      usernameValid === false ||
+      emailValid === false ||
+      passValid === false
+    ) {
+      return setSignupAlert(true);
+    }
+
+    setSignupAlert(false);
+  };
   return (
-    <form onSubmit={handleSubmit}>
-      <input placeholder="email" name="email" onChange={handleInputChange} />
-      <input
-        placeholder="password"
-        name="password"
-        onChange={handleInputChange}
-      />
-      <input type={"submit"} value="Submit" onSubmit={handleSubmit} />
-    </form>
+    <>
+      <h2>Login</h2>
+      <form onSubmit={handleLoginSubmit}>
+        <input
+          placeholder="Email"
+          name="email"
+          onChange={handleInputChange}
+          onBlur={validate}
+        />
+        <input
+          placeholder="Password"
+          type={"password"}
+          name="password"
+          onChange={handleInputChange}
+          onBlur={validate}
+        />
+        <input type={"submit"} value="Submit" />
+        {showLoginAlert && <h3 style={{ color: "red" }}>{alertMsg}</h3>}
+      </form>
+
+      <h2>Sign Up</h2>
+      <form onSubmit={handleSignupSubmit}>
+        <input
+          placeholder="Username"
+          name="username"
+          value={formData.username}
+          onChange={handleInputChange}
+          onBlur={validate}
+        />
+        <input
+          placeholder="Email"
+          name="email"
+          value={formData.email}
+          onChange={handleInputChange}
+          onBlur={validate}
+        />
+        <input
+          placeholder="Password"
+          type={"password"}
+          name="password"
+          value={formData.password}
+          onChange={handleInputChange}
+          onBlur={validate}
+        />
+        <input
+          placeholder="Confirm Password"
+          type={"password"}
+          name="confirm password"
+          onBlur={validate}
+        />
+        <input type={"submit"} value="Submit" />
+        {showSignupAlert && (
+          <h3 style={{ color: "red" }}>
+            Please enter a valid Username/Email/Password
+          </h3>
+        )}
+      </form>
+    </>
   );
 }
