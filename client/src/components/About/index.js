6ae1ccb300ab73809184bd366946e03dc930a09f
Christopher <leechrs1@gmail.com>
2022-08-06 16:20:56 -0500

    working on addUSer

--- client/src/components/About/index.js
+++ client/src/components/About/index.js
@@ -1,7 +1,26 @@
 import React from 'react';
 import coverImage from  '../../assets/cover-image.jpeg';
+import { useMutation } from '@apollo/client';
+import { ADD_USER } from '../../utils/mutations'
 
 function About() {
+
+    // ADD USER
+    const [addUser, {error}] = useMutation(ADD_USER);
+    
+    const handleFormSubmit = async event => {
+        event.preventDefault();
+
+        try {
+            const { data } = await addUser({
+                variables: { ...formState }
+            });
+            console.log(data)
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
     return (
         <section>
             <form class="login-form">
