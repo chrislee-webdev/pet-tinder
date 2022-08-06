63f3c42ae25068f371d142505afb9d4c22bf992e
CarlJMcGee <98133059+CarlJMcGee@users.noreply.github.com>
2022-08-06 09:45:06 -0500

    added user and pets data query to FindPetPal comp

--- client/src/components/FindPetPal/index.js
+++ client/src/components/FindPetPal/index.js
@@ -2,51 +2,70 @@
 
 // import React
 import React, { useState } from "react";
-import TinderCard from 'react-tinder-card';
+import TinderCard from "react-tinder-card";
 //import userData from '../../../../server/seed/userData.json'
 import { useQuery, useMutation } from "@apollo/client";
-
-//TODO: get user data 
- const db = userData
+import { ME, PETS } from "../../utils/queries";
 
 function FindPetPal() {
-    const pets = db;
-    const [lastDirection, setLastDirection] = useState()
+  //TODO: get user data
+  const {
+    loading: gettingUser,
+    data: userData,
+    error: UserDataErr,
+  } = useQuery(ME);
+  const {
+    loading: gettingPets,
+    data: petData,
+    error: PetDataErr,
+  } = useQuery(PETS);
+  const [lastDirection, setLastDirection] = useState();
 
+  if (gettingUser || gettingPets) {
+    return <h1>Loading...</h1>;
+  }
 
-    const swipe = (direction, nameToDelete) => {
-        console.log('removing: ' + nameToDelete) 
-        setLastDirection(direction)
-    }
+  const { me } = userData;
+  const { pets } = petData;
 
-    const outOfFrame = (name) => {
-        console.log (name + 'left the screen!')
-    }
+  const swipe = (direction, nameToDelete) => {
+    console.log("removing: " + nameToDelete);
+    setLastDirection(direction);
+  };
 
+  const outOfFrame = (name) => {
+    console.log(name + "left the screen!");
+  };
 
-    return (
-        <section>
-            <h1>Find a pal for your pet!</h1>
-            <img src={require('../../assets/pals.jpeg')}
-                alt="puppies"
-            />
-            <div className='cardContainer'>
-        {pets.map((character) =>
-          <TinderCard className='swipe' key={pets.name} onSwipe={(dir) => swipe(dir, pets.name)} onCardLeftScreen={() => outOfFrame(pets.name)}>
-            <div style={{ picture: 'url(' + character.url + ')' }} className='card'>
+  return (
+    <section>
+      <h1>Find a pal for your pet!</h1>
+      <img src={require("../../assets/pals.jpeg")} alt="puppies" />
+      <div className="cardContainer">
+        {pets.map((character) => (
+          <TinderCard
+            className="swipe"
+            key={pets.name}
+            onSwipe={(dir) => swipe(dir, pets.name)}
+            onCardLeftScreen={() => outOfFrame(pets.name)}
+          >
+            <div
+              style={{ picture: "url(" + character.url + ")" }}
+              className="card"
+            >
               <h3>{pets.name}</h3>
             </div>
           </TinderCard>
-        )}
+        ))}
       </div>
-      {lastDirection ? <h2 className='infoText'>You swiped {lastDirection}</h2> : <h2 className='infoText' />}
-
-    
+      {lastDirection ? (
+        <h2 className="infoText">You swiped {lastDirection}</h2>
+      ) : (
+        <h2 className="infoText" />
+      )}
     </section>
-  )
+  );
 }
-//TODO: add pass and like buttons 
-
-
+//TODO: add pass and like buttons
 
 export default FindPetPal;
