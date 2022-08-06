2942b48a2d3740eca98f4c74675ba3c53bd94a72
rgiinther <r.giin95@gmail.com>
2022-08-06 14:08:34 -0500

    updated Nav index with login button

--- client/src/components/navigation/index.js
+++ client/src/components/navigation/index.js
@@ -2,31 +2,37 @@
 
 // import React
 import React from "react";
+import '../../styles/Navigation.css';
 
 // Navigation function
 function Navigation({ currentPage, handlePageChange }) {
     return (
-        <header>
-            <h1>Pet Tinder</h1>
+        <header className='header'>
+            <h1 className="webTitle">Pinder: Puppy Love</h1>
 
             <nav>
-                <ul>
-                    <li className="mx-2">
-                        <a onClick={() => handlePageChange('About')}
+                <ul className='btnContainer'>
+                    <li className="mx-2 LoginBtn">
+                        <span onClick={() => handlePageChange('LoginPage')}
+                        className={currentPage === 'LoginPage' ? 'nav-link active' : 'nav-link'}>Login or Sign up</span>
+                    </li>
+                    <li className="mx-2 aboutBtn ">
+                        <span onClick={() => handlePageChange('About')}
                         className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
-                        >Homepage</a>
+                        >Homepage</span>
                     </li> 
 
-                    <li className="mx-2">
+                    <li className="mx-2 addPetBtn">
                         <span onClick={() => handlePageChange('AddPet')}
                         className={currentPage === 'AddPet' ? 'nav-link active' : 'nav-link'}>Add a pet</span>  
                     </li>
 
-                    <li className="mx-2">
+                    <li className="mx-2 findPetPalBtn">
                         <span onClick={() => handlePageChange('FindPetPal')}
                         className={currentPage === 'FindPetPal' ? 'nav-link active' : 'nav-link'}
                         >Find pet pal</span>
                     </li>
+                   
                 </ul>
             </nav>
         </header>
