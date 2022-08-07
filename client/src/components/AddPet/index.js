d6e566741a48388426f51002e6dfbf8a3722bb0a
rgiinther <r.giin95@gmail.com>
2022-08-06 20:49:14 -0500

    added mutations to AddPet

--- client/src/components/AddPet/index.js
+++ client/src/components/AddPet/index.js
@@ -1,7 +1,9 @@
-import React from "react";
+import { useMutation } from "@apollo/client";
+import React, { useState } from "react";
 import Select from "react-select";
 import '../../styles/AddPet.css';
 import { ADD_PET } from '../../utils/mutations';
+import Auth from '../../utils/auth';
 
 const Age = [
   { label: "0 - 1", value: 1 },
@@ -31,28 +33,72 @@ const Breed = [
   { label: "Poodle", value: 4 },
   { label: "Samoyed", value: 5 },
 ];
+const AddPet = () => {
+  const [addPetData, setAddPetData] = useState({ 
+    age: '', 
+    gender: '', 
+    temper: '', 
+    breed: '',
+  });
 
-function AddPet() {
+
+  const [addPet, { error }] = useMutation(ADD_PET);
+
+
+  const handleChange = async (event) => {
+    const { name, value } = event.target;
+    setAddPetData({
+      ...addPetData, 
+      [name]: value,
+    })
+  };
+
+//submit form
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+
+  try {
+    const { data } = await addPet({
+      variables: { ...addPetData}
+    })
+    console.log(data);
+    Auth.login(data.addPet.token)
+  }catch (e) {
+    console.error(e);
+  }
+    setAddPetData({
+      age:'',
+      gender: '',
+      temper: '',
+      breed: '',
+    });
+  };
+
   return (
+ 
     <section className='addPetContainer'>
       <h1>Create a pet profile</h1>
-
+      <form onSubmit={handleFormSubmit}>
       <div>
+        Pet Name: 
+      </div>
+      <div value={addPetData.breed} onChange={handleChange}>
         Breed: <Select options={Breed} />
       </div>
 
-      <div>
+      <div value={addPetData.gender} onChange={handleChange}>
         Gender: <Select options={Gender} />
       </div>
 
-      <div>
+      <div value={addPetData.age} onChange={handleChange}>
         Age: <Select options={Age} />
       </div>
 
-      <div>
+      <div value={addPetData.temper} onChange={handleChange}>
         Temperament: <Select options={Temper} />
       </div>
-      <button className="btn">Submit</button>
+      <button className="btn" type='submit'>Submit</button>
+      </form> 
     </section>
   );
 }
